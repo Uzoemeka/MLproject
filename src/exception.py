be5adc55adc85fe778cfcb909560c2b70748c498
Uzoemeka <uzochukwuemeka@gmail.com>
2025-03-17 13:34:38 +0000

    EDA analysis

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail:sys):
     _,_,exc_tb = error_detail.exc_info()
@@ -26,7 +26,6 @@ class CustomException(Exception):
 #     except Exception as e:
 #         logging.info("Divde by Zero error")
 #         raise CustomException(e,sys)
-            
 
         
             
